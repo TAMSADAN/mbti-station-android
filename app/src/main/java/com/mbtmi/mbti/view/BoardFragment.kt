38497711d56be256031c9d1fb6e26804b498128c
Seungmin <tmdals8798@gmail.com>
2022-07-03 12:37:20 +0900

    [style] Change Font & Board View

--- app/src/main/java/com/mbtmi/mbti/view/BoardFragment.kt
+++ app/src/main/java/com/mbtmi/mbti/view/BoardFragment.kt
@@ -4,6 +4,8 @@ import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
 import com.mbtmi.mbti.R
@@ -12,6 +14,7 @@ import com.mbtmi.mbti.databinding.FragmentBoardBinding
 import kotlin.math.abs
 
 // 2022.06.26 신승민 작성
+// 2022.07.01 신승민 수정
 class BoardFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_board){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -20,21 +23,23 @@ class BoardFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_board
         initScrollChange()
     }
 
+    // scroll 할 때 변하는 offset을 이용해서 상단 view 변하도록 하는 함수
     private fun initScrollChange(){
         val toolbarConstraintLayout = binding.toolbarConstraintLayout
         val appBarLayout = binding.appBarLayout
+        val menuConstraintLayout = binding.menuConstraintLayout
 
         toolbarConstraintLayout.alpha = 0.0f
 
         appBarLayout.addOnOffsetChangedListener(OnOffsetChangedListener { appBarLayout, verticalOffset ->
             val totalOffset = appBarLayout.totalScrollRange
             val offset = abs(verticalOffset)
-            var ratio = 0.0f // 0: 펼침
+            var ratio = 0.0f
             if (offset != 0) {
                 ratio = offset.toFloat() / totalOffset.toFloat()
             }
             toolbarConstraintLayout.alpha = (ratio - 0.5f) * 2 + 0.1f
-            Log.d(TAG, "initScrollChange: $ratio")
+            menuConstraintLayout.alpha = (ratio - 0.5f) * (-2.0f)
         })
     }
 }
